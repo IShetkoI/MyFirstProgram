d2014f75df909a1e80f569538262e0c16255e908
IShetkoI <vanya.shetko38@gmail.com>
2022-07-25 06:05:52 +0300

    something scary

---
+++ cmake-build-debug/CMakeFiles/MyFirstProgram.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MyFirstProgram.dir/main.c.obj"
+  "CMakeFiles/MyFirstProgram.dir/main.c.obj.d"
+  "MyFirstProgram.exe"
+  "MyFirstProgram.exe.manifest"
+  "MyFirstProgram.pdb"
+  "libMyFirstProgram.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/MyFirstProgram.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/MyFirstProgram.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for MyFirstProgram.
+# This may be replaced when dependencies are built.
